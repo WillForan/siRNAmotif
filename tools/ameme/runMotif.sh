3310c64e2577d9c0722d58b941591e657f4e309a
Will Foran <wforan1@andrew.cmu.edu>
2011-05-19 14:58:58 -0400

    made tool directory to unclutter (change relaitve paths too)

--- tools/ameme/runMotif.sh
+++ tools/ameme/runMotif.sh
@@ -9,21 +9,21 @@ export RUNS=4
 
 function runmeme() {
     echo "=====Control Run 1====="
-    export RANDFILE=../fas/randomIntergenic-2011-05-12-14\:24.fa 
-    export STARTFILE=../fas/randomIntergenic-2011-05-12-14\:27.fa 
+    export RANDFILE=../../fas/randomIntergenic-2011-05-12-14\:24.fa 
+    export STARTFILE=../../fas/randomIntergenic-2011-05-12-14\:27.fa 
     ./recurse_motif.sh
 
-    export RANDFILE=../fas/randomIntergenic-2011-05-12-14.fa 
+    export RANDFILE=../../fas/randomIntergenic-2011-05-12-14.fa 
     echo "=====Control Run 2====="
-    export STARTFILE=../fas/randomIntergenic-2011-05-12-14\:24.fa 
+    export STARTFILE=../../fas/randomIntergenic-2011-05-12-14\:24.fa 
     ./recurse_motif.sh
 
     echo "=====Control Run 3====="
-    export STARTFILE=../fas/randomIntergenic-2011-05-12-14\:27.fa 
+    export STARTFILE=../../fas/randomIntergenic-2011-05-12-14\:27.fa 
     ./recurse_motif.sh
 
     echo "====Actual===="
-    export STARTFILE=../fas/300_top.fa
+    export STARTFILE=../../fas/300_top.fa
     ./recurse_motif.sh
 }
 export AMEMEOPTIONS="background=m2 gif=/dev/null numMotifs=2 controlRun=on"
--- tools/projections/runProjection.sh
+++ tools/projections/runProjection.sh
@@ -1,4 +1,4 @@
-for seq in ../fas/{randomIntergenic-2011-05-12-14.fa,gt4TATCATTTTT.fa,300_top.fa}; do
+for seq in ../../fas/{randomIntergenic-2011-05-12-14.fa,gt4TATCATTTTT.fa,300_top.fa}; do
     for len in {7..15}; do
 	projection-toolkit-0.42//findmotif -s $seq -l $len  2>/dev/null | 
 	grep 'CON\|Info' |
