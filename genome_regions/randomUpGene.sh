d7b8f50d7ec0cca7fe8db86d7f007f1056231d41
Will Foran <wforan1@andrew.cmu.edu>
2011-06-08 10:06:03 -0400

    'upgeme 1000 nts'

--- genome_regions/randomUpGene.sh
+++ genome_regions/randomUpGene.sh
@@ -2,34 +2,41 @@
 
 file=gentic_region.txt
 numseqs=300
-seqlength=101
+seqlength=1000
 
-while read chrom start end strand; do
- 
- 
+i=0;
 
- 
- if [ $start -lt $seqlength ];then  echo 'starts too close to beginning' 1>&2; continue; fi;
+shuffle $file | while read chrom start end strand; do
 
- #give seq a name (genomic position, direction, start
- echo ">$chrom:$start-${end}($strand)"
+    #if we've done more then enough end
+    if [ $i -gt $numseqs ]; then break; fi
+
+    #can't get 1000 upstream of this 
+    if [ $start -lt $seqlength ];then  echo 'starts too close to beginning' 1>&2; continue; fi;
+
+    #count it
+    $i=$(($i+1));
+
+
+    #give seq a name (genomic position, direction, start
+    echo ">$chrom:$start-${end}($strand)"
 
 
 
- if [ $strand == '+' ]; then
- #    (5')       s--------------e#  sequence
- #|--seqlen-----|                #  taken
-     start=$(($start-$seqlength-1))
-     end=$(($start+$seqlength))
- else
- #s-------------e  (5')          #  sequence
- #               |e---seqlen---s|#  taken
+    if [ $strand == '+' ]; then
+	#    (5')       s--------------e#  sequence
+	#|--seqlen-----|                #  taken
+	start=$(($start-$seqlength-1))
+	end=$(($start+$seqlength))
+    else
+	#s-------------e  (5')          #  sequence
+	#               |e---seqlen---s|#  taken
 
-     end=$(($end+1))
-     start=$(($end+$seqlength))
+	end=$(($end+1))
+	start=$(($end+$seqlength))
 
- fi
- 
- pos2seq.pl chr$chrom:$start-$end
+    fi
 
-done < <(shuffle $file|head -n $numseqs)
+    pos2seq.pl chr$chrom:$start-$end
+
+done 
