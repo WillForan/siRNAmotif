ef5ac4cb5bfd160a60dca5a83a78d1b4db8289ef
Will Foran <wforan1@andrew.cmu.edu>
2011-06-08 12:48:55 -0400

    canidateFasta now also makes bed file

--- genome_regions/candidateFasta.sh
+++ genome_regions/candidateFasta.sh
@@ -9,6 +9,9 @@
 PAD=1000
 DB=/home/wforan1/seq/srna/data/srna.db
 POS2SEQ=/home/RNA/PlasmodiumFalciparum/genome/pos2seq.pl
+BEDFILE=../fas/300_top_$PAD.bed
+FAFILE=../fas/300_top_$PAD.fa
+rm $BEDFILE $FAFILE;
 
 sqlite3 -separator ' ' $DB  'select chrom,start,end,strand,cov,antiGene from candidates where length>18 and length<24 and antiGene not like "%RNA%" order by maxcov desc limit 300 ' |
 while read chrom start end strand cov ag; do
@@ -23,17 +26,25 @@ while read chrom start end strand cov ag; do
   #
      start=$end
      s=$(($end+$PAD))
+     strand='-'
   else
   #
   # s   start    gene
   # |----|>>>>>>......
   #
       s=$(($start-$PAD))
+      strand='+'
       #if we're less than 0
       if [ "${s:0:1}" == "-" ]; then s=0; fi;
   fi
 
-  echo ">($strand-$cov-$ag)chr$chrom:$start-$end"
-  $POS2SEQ chr$chrom:$s-$start
-done |tee ../fas/300_top_$PAD.fa
+  echo ">($strand-$cov-$ag)chr$chrom:$start-$end"	>> $FAFILE
+  $POS2SEQ chr$chrom:$s-$start				>> $FAFILE
+  if [ $strand = '+' ]; then
+      echo $chrom $s $start chr$chrm:$start-$end-$cov-$ag $strand $s $start 255,0,0 >> $BEDFILE
+  else
+      echo $chrom $start $s chr$chrm:$start-$end-$cov-$ag $strand $start $s 255,0,0 >> $BEDFILE
+  fi
+
+done 
 
