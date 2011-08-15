64b6ea295c25acfd1d61bace130ab6971aebb3b6
Will Foran <wforan1@andrew.cmu.edu>
2011-08-15 14:31:36 -0400

    getMotifs header includes motif

--- tools/decod/output/getMotifs.pl
+++ tools/decod/output/getMotifs.pl
@@ -11,6 +11,7 @@ getopts('w:r:f:d:',\%opts);
 
 my $score=0;
 my $totalcount=1;
+my %letter=(0 =>'A', 1 =>'C', 2 =>'G',3 =>'T');
 #for my $width (4..9){
 for my $width (split /,/,$opts{w}){
     #for my $randfile (29,35){
@@ -24,15 +25,38 @@ for my $width (split /,/,$opts{w}){
 		#chomp;
 		#push my @lines, $_;
 		my @lines;
+
+		my @motifarray;
+		my $motif='';
 		while(<$motifFH>){
 		    chomp;
 		    last if ! m/^[ATGC]/;
 		    push @lines, $_;
+		    
+		    #see flatten.pl for whats goign on here
+		    my $idx=0;
+		    while(m/(\d+.?\d+)/g){
+			push @{$motifarray[$idx]}, $&;
+			$idx++;
+		    }
+		}
+		#see flatten.pl for whats goign on here
+		for my $pos (0..$#motifarray){
+		    for my $nt (0..3) {
+
+			my $freq = sprintf ( "%2.0f", $motifarray[$pos]->[$nt]*100);
+
+			if($freq > 0){
+			    $motif.= $freq if $freq<100; 
+			    $motif.= $letter{$nt} 
+			}
+		    }
+		    $motif.="_" unless $pos==$#motifarray;
 		}
 		m/^#Score = (\d+.\d+(E-\d+)?)/;
 		#$lines[0].="_".substr(1*$1,0,6)."_w${width}_$randfile";
 
-		print ">${totalcount}-M${count}_",sprintf("%.5f",$1),"_w${width}_$randfile\n";
+		print ">$randfile-${width}-${count}-$motif-",sprintf("%.5f",$1),"\n";
 		print join("\n",@lines),"\n";
 		$count++;
 		$totalcount++;
